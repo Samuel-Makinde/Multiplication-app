5fc945daefe97a7c0f31e25a26cc4f360b1e0880
Samuel Makinde <makindesamuel1999@gmail.com>
2023-01-15 15:08:34 +0100

    new update

---
+++ app.js
@@ -0,0 +1,33 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+questionEl.innerText = `what is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+let score = JSON.parse(localStorage.getItem("score"));
+formEl.addEventListener("submit", ()=>{
+    const userAns = +inputEl.value;
+    if(userAns === correctAns){
+        score++;
+        addLocalStorage();
+    } else{
+        score--
+        addLocalStorage();
+    }
+})
+
+function addLocalStorage (){
+    localStorage.setItem("score", JSON.stringify(score));
+}
+
+if(!score){
+    score = 0;
+}
+scoreEl.innerText = `score: ${score}`;
+// if(!userAns){
+//     score = 0;
+//     alert("You did not provide any answer!");
+// }
